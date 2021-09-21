0dd8e551628edc1da50616c6046095f4b614e7b4
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-09-21 11:18:42 -0300

    testing Promises and using after and before methods

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -34,3 +34,44 @@ test('Probar un callback',()=>{
     expect(str).toBe('aloH')
   })
 })
+
+// Probando promesas
+const reverseString2 = str => {
+  return new Promise((resolve, reject)=>{
+    if(!str){
+      reject(Error('Error'))
+    }
+    resolve(str.split("").reverse().join(""))  
+  })
+}
+
+test('Probar nuestra promesa',()=>{
+  return reverseString2('Hola')
+  .then(string=>{
+    expect(string).toBe('aloH')
+  })
+})
+
+// Probar Async/Await
+test('Probar nuestra promesa',async ()=>{
+  const string = await reverseString2('Hola')
+  expect(string).toBe('aloH')
+})
+
+// Funciones que se ejecutaran antes o despues de cada prueba
+// Útil por ejemplo para montar y desmontar una base de datos y testearla.
+afterEach(()=>{
+  console.log('Despues de cada prueba')
+})
+
+afterAll(()=>{
+  console.log('Despues de todas las pruebas')
+})
+
+beforeEach(()=>{
+  console.log('Antes de cada prueba')
+})
+
+beforeAll(()=>{
+  console.log('Antes de todas las pruebas')
+})
